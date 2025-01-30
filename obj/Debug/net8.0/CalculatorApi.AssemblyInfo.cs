6e410b39ebc885ef6d8d97a94351d766eed358fb
duarte <Somsenduarte@gmail.com>
2025-01-30 20:17:10 +0100

    first commit

--- obj/Debug/net8.0/CalculatorApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/CalculatorApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a29946463982166a83b65c0135f16d8ee1b3272e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f593490e92b86f138ef2cc7811716569c6bca3e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
